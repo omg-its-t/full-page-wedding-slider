5ac8868aae79099db867bc6ed6f2e7194ff78648
Mellske <blackvq35@gmail.com>
2019-04-04 16:20:49 -0500

    first commit - previous slide not working yet

---
+++ main.js
@@ -0,0 +1,50 @@
+const slide = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+function nextSlide(){
+    //get the class current
+    const current = document.querySelector('.current');
+    //remove the current class
+    current.classList.remove('current');
+
+    //check for next slide
+    if(current.nextElementSibling){
+        //if yes add to next sibling
+        current.nextElementSibling.classList.add('current');
+    }
+    else{
+        //put current class on start
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+function prevSlide(){
+    //get the class current
+    const current = document.querySelector('.current');
+    //remove the current class
+    current.classList.remove('current');
+
+    //check for previous slide
+    if(current.previousElementSibling){
+        //if yes add to previous sibling
+        current.previousElementSibling.classList.add('current');
+    }
+    else{
+        //put current class on last slide
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+next.addEventListener('click', e => {
+    nextSlide();
+});
+
+prev.addEventListener('click', e => {
+    prevSlide();
+});
